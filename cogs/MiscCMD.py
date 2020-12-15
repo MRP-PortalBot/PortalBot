55f0fa7f68461fc9362c77e7a8ae0fd6c19e40a0
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2020-12-14 21:50:28 -0500

    Update MiscCMD.py

--- cogs/MiscCMD.py
+++ cogs/MiscCMD.py
@@ -1,16 +1,8 @@
 import discord
-import flask
-import keep_alive
-import logging
 from discord.ext import commands
-import json 
-import time
-import datetime
-from datetime import timedelta, datetime
-import io
-import contextlib
 from discord import Webhook, AsyncWebhookAdapter
 import aiohttp
+
 
 rules = [":one: **No Harassment**, threats, hate speech, inappropriate language, posts or user names!", ":two: **No spamming** in chat or direct messages!", ":three: **No religious or political topics**, those don’t usually end well!", ":four: **Keep pinging to a minimum**, it is annoying!", ":five: **No sharing personal information**, it is personal for a reason so keep it to yourself!", ":six: **No self-promotion or advertisement outside the appropriate channels!** Want your own realm channel? **Apply for one!**", ":seven: **No realm or server is better than another!** It is **not** a competition.", ":eight: **Have fun** and happy crafting!", ":nine: **Discord Terms of Service apply!** You must be at least **13** years old."]
 
@@ -19,6 +11,7 @@ class MiscCMD(commands.Cog):
   def __init__(self,bot):
     self.bot = bot
 
+  #DM Command
   @commands.command()
   @commands.has_role("Moderator")
   async def DM(self, ctx, user: discord.User, *, message=None):
@@ -35,6 +28,7 @@ class MiscCMD(commands.Cog):
     if isinstance(error, commands.MissingRole):
       await ctx.send("Uh oh, looks like you don't have the Moderator role!")
 
+  #Ping Command
   @commands.command()
   async def ping(self, ctx):
     author = ctx.message.author
@@ -46,6 +40,7 @@ class MiscCMD(commands.Cog):
     pingembed.add_field(name = "Current Ping:" , value = f'{round(self.bot.latency * 1000)}ms')
     await ctx.send(embed = pingembed)
 
+  #Uptime Command
   @commands.command()
   async def uptime(self,ctx):
     author = ctx.message.author
@@ -54,7 +49,7 @@ class MiscCMD(commands.Cog):
     logfile.close()
     await ctx.send("Really long time, lost track. ")
 
-
+  #Purge Command
   @commands.command()
   @commands.has_permissions(manage_messages = True)
   async def clear(self, ctx,amount=2):
@@ -64,6 +59,7 @@ class MiscCMD(commands.Cog):
     logfile.close()
     await ctx.channel.purge(limit = amount)
 
+  #Say Command
   @commands.command()
   @commands.has_permissions(manage_channels = True)
   async def say(self, ctx,*,reason):
@@ -74,6 +70,7 @@ class MiscCMD(commands.Cog):
     await ctx.channel.purge(limit = 1)
     await ctx.send(reason) 
 
+  #Embed Command
   @commands.command()
   @commands.has_permissions(manage_channels = True)
   async def embed(self, ctx, channel : discord.TextChannel, color : discord.Color , *, body):
@@ -85,7 +82,7 @@ class MiscCMD(commands.Cog):
     embed = discord.Embed(title = title, description = bottom, color = color)
     await channel.send(embed = embed)
     
-
+  #Nick Commamd
   @commands.command()
   @commands.has_role("Moderator")
   async def nick(self, ctx, user :discord.Member, channel : discord.TextChannel):
@@ -107,7 +104,7 @@ class MiscCMD(commands.Cog):
     if isinstance(error, commands.MissingRole):
       await ctx.send("Uh oh, looks like you don't have the Moderator role!")
 
-
+  #Removes your nickname. 
   @commands.command()
   async def rememoji(self, ctx):
     author = ctx.message.author
@@ -118,6 +115,7 @@ class MiscCMD(commands.Cog):
     await author.edit(nick = str(author.name))
     await ctx.send("Removed your nickname!")
 
+  #Add's an emoji to your nickname.
   @commands.command()
   async def addemoji(self, ctx, channel : discord.TextChannel):
     author = ctx.message.author
@@ -138,7 +136,7 @@ class MiscCMD(commands.Cog):
     if isinstance(error, commands.BadArgument):
       await ctx.send("Hmm, you didn't give me all the arguments.")
 
-
+  #Rule Command [INT]
   @commands.command()
   async def rule(self, ctx,*,number):
     author = ctx.message.author
@@ -148,6 +146,7 @@ class MiscCMD(commands.Cog):
     await ctx.send(rules[int(number)-1])
 
 
+  #Add's a gamertag to the database. 
   @commands.command()
   async def gamertag(self, ctx, gamertag):
     author = ctx.message.author
@@ -175,6 +174,7 @@ class MiscCMD(commands.Cog):
       await ctx.send("Uh oh, you didn't include all the arguments! ")
 
 
+  #Tag command, extra commands basically. 
   @commands.command()
   async def tag(self, ctx, tagnum):
     if tagnum == "1":
@@ -184,8 +184,10 @@ class MiscCMD(commands.Cog):
       await ctx.send(embed = contentc)
     elif tagnum == "2":
       text_channel2 = self.bot.get_channel("587850399759990794")
-      realm = discord.Embed(title = "Realm Applications", description = f"Welcome! I see you've asked about joining a realm! The Realm Portal has a ton of realms you can choose from. Each having their own application process, if you would like to join one check out the Realms and Server tab! You can view details about each reakm by checking its pings and channel description! Please remember that there is no 'better' realm! \n**Community Realm** Don't know what to join? Consider joining the MRP Community Realm! \n**Apply for a Realm Channel!** Have a realm you would like to advertise? Once you reach Zombie Slayer, you should be able to fill out the application in <#587850399759990794>!", color = 0xeb07cc)
+      realm = discord.Embed(title = "Realm Applications", description = f"Welcome! I see you've asked about joining a realm! The Realm Portal has a ton of realms you can choose from. Each having their own application process, if you would like to join one check out the Realms and Server tab! You can view details about each realm by checking its pins and channel description! Please remember that there is no 'better' realm! \n**Community Realm** Don't know what to join? Consider joining the MRP Community Realm! \n**Apply for a Realm Channel!** Have a realm you would like to advertise? Once you reach Zombie Slayer, you should be able to fill out the application in <#587850399759990794>!", color = 0xeb07cc)
       await ctx.send(embed = realm)
+    elif tagnum == "3":
+      realm = discord.Embed(title = "Realm Applications", description = "Hello! It looks like you wanted to join a realm! Please not that all realms that are shown in the Realm Portal are **BEDROCK**! If you would like to join a realm, please take a moment and read the channel's description **and** the pins! You can find things like their application, realm details, etc in there. \n**NOTE:** Please do not contact Moderators or Admins if you have a question regarding a realm. It's best to contact the realm owners/operators as we are not a representative for that specific realm! ")
     
     elif tagnum == "help":
       await ctx.send("**Tag Numbers:** \n1: Content Related \n2: Realm Applications")
@@ -193,54 +195,16 @@ class MiscCMD(commands.Cog):
     else:
       await ctx.send("That number isn't a valid response!")
 
-
-  @commands.command()
-  @commands.is_owner()
-  async def loadbeta(self, ctx):
-    await ctx.send("Please wait, loading `BETA` **Extensions...**") 
-    msg = await ctx.send("`[#---------]`")
-    time.sleep(1)
-    await msg.edit(content = "`[##--------]`")
-    time.sleep(1)
-    await msg.edit(content = "`[###-------]`")
-    time.sleep(1)
-    await msg.edit(content = "`[####------]`")
-    time.sleep(2)
-    await msg.edit(content = "`[#####-----]`")
-    time.sleep(1)
-    await msg.edit(content = "`[######----]`")
-    time.sleep(3)
-    await msg.edit(content = "`[#######---]`")
-    time.sleep(1)
-    await msg.edit(content = "`[########--]`")
-    time.sleep(2)
-    await msg.edit(content = "`[#########-]`")
-    time.sleep(1)
-    await msg.edit(content = "`[##########]`")
-    time.sleep(3)
-    await msg.edit(content = "`COMPLETE`")
-    self.bot.load_extension("cogs.BetaCMD")
-  
-  @commands.command()
-  @commands.is_owner()
-  async def unloadbeta(self, ctx):
-    await ctx.send("Unloaded `BETA` and all of its **Extensions**.")
-    self.bot.unload_extension("cogs.BetaCMD")
-
+  #OfO command. 
   @commands.command()
   async def webhook(self, ctx, *, reason):
+    return
     async with aiohttp.ClientSession() as session:
       url = 'https://discord.com/api/webhooks/783135151155970049/nTOU4H3ch2Q3Z3lLEDUGj8jq-tNZ-cZFRvBPdjphl5aMYtM5j3Urv7p1KtTMxXfrwZmo'
       webhook = Webhook.from_url(url, adapter=AsyncWebhookAdapter(session)) #something here to cycle through url's
       author = ctx.message.author
       await webhook.send(reason ,username=author.name, avatar_url = author.avatar_url)
 
-
-
-
-
-
- 
-
+  
 def setup(bot):
   bot.add_cog(MiscCMD(bot))
