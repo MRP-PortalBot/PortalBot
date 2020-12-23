88862b9c5554132bb73bf0e512e335e60b8e1112
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2020-12-22 19:48:45 -0500

    Update BlacklistCMD.py

--- cogs/BlacklistCMD.py
+++ cogs/BlacklistCMD.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 import time
-
+import re
+import asyncio
+from discord import Embed
 #--------------------------------------------------
 #pip3 install gspread oauth2client
 
@@ -27,27 +29,43 @@ print("Done.")
 cell = sheet.cell(3,1).value
 print(cell)
 '''
+
+gtsheet = client.open("Gamertag Data").sheet1
+#3 Values to fill
+
+#Template on modfying spreadsheet
+'''
+gtrow = ["1", "2", "3"]
+gtsheet.insert_row(row, 3)  
+print("Done.")
+
+gtcell = sheet.cell(3,1).value
+print(cell)
+'''
 #-----------------------------------------------------
 
 
-Q1 = "User's Discord: (Please don't include the last 4 tag numbers!)"
-Q2 = "User's Tag: (Example: #1086)"
-Q3 = "User's Discord Long ID: "
-Q4 = "User's Gamertag: "
-Q5 = "Banned from (realm): "
-Q6 = "Known Alts: "
-Q7 = "Reason for Ban: "
-Q8 = "Date of Incident"
-Q9 = "The User has faced a (Temporary/Permanent) ban: "
-Q10 = "If the ban is Temporary, the ban ends on: "
+Q1 = "User's Discord: "
+#Savaged, changed the question so it doesn't say not to include the tag number here
+Q2 = "User's Discord Long ID: "
+Q3 = "User's Gamertag: "
+Q4 = "Banned from (realm): "
+Q5 = "Known Alts: "
+Q6 = "Reason for Ban: "
+Q7 = "Date of Incident"
+Q8 = "The User has faced a (Temporary/Permanent) ban: "
+Q9 = "If the ban is Temporary, the ban ends on: "
 
 QQ1 = "What should I open for you? \n >  **Options:** `Gamertag` / `Discord` / `Combined`"
 a_list = []
 
 
+
 class BlacklistCMD(commands.Cog):
   def __init__(self,bot):
     self.bot = bot
+
+
 
   #Starts the blacklist process.
   @commands.command()
@@ -59,7 +77,7 @@ class BlacklistCMD(commands.Cog):
     channel = await ctx.author.create_dm()
     #schannel = self.bot.get_channel(778453455848996876)
     
-    schannel = self.bot.get_channel(778453455848996876)
+    schannel = self.bot.get_channel(590226302171349003)
     await ctx.send("Please take a look at your DM's!")
    
 
@@ -95,12 +113,10 @@ class BlacklistCMD(commands.Cog):
     await channel.send(Q9)
     answer9 = await self.bot.wait_for('message', check=check)
 
-    await channel.send(Q10)
-    answer10 = await self.bot.wait_for('message', check=check)
     time.sleep(0.5)
 
     #Spreadsheet Data
-    row = [answer1.content, answer2.content, answer3.content, answer4.content, answer5.content, answer6.content, answer7.content, answer8.content, answer9.content, answer10.content]
+    row = [answer1.content, answer2.content, answer3.content, answer4.content, answer5.content, answer6.content, answer7.content, answer8.content, answer9.content]
     sheet.insert_row(row, 3)  
 
     submit_wait = True
@@ -124,7 +140,8 @@ class BlacklistCMD(commands.Cog):
         await schannel.send(embed = blacklistembed)
         await channel.send("I have sent in your blacklist report, thank you! \n**Response Record:** https://docs.google.com/spreadsheets/d/1WKplLqk2Tbmy_PeDDtFV7sPs1xhIrySpX8inY7Z1wzY/edit#gid=0&range=D3 \n*Here is your cookie!* 🍪")
       elif "break" in msg.content:
-        schannel.send("Canceled Request...")
+        await channel.send("Canceled Request...")
+        await ctx.send("Canceled Request...")
         submit_wait = False
           
   
@@ -134,14 +151,21 @@ class BlacklistCMD(commands.Cog):
       await ctx.send("Uh oh, looks like you don't have the Realm OP role!")
   
   @commands.command()
+  @commands.has_role("Realm OP")
   async def bsearch(self, ctx, * , username):
     checkcheck = "FALSE"
     author = ctx.message.author
     em = discord.Embed(title = "Google Sheets Search", description = "Requested by Operator " + author.mention, color = 0x18c927)
-    values = sheet.findall(username)
+    #values_re = re.compile(r'(?i)' + username)
+    #print(values_re)
+    #'re.Pattern' object is not iterable
+    #values = sheet.findall(username)
+    values_re = re.compile(r'(?i)' + '(?:' + username + ')')
+    print(values_re)
+    values = sheet.findall(values_re)
     print(values)
     try:
-      checkempty = ', '.join(sheet.row_values(sheet.find(username).row))
+      checkempty = ', '.join(sheet.row_values(sheet.find(values_re).row))
       print(checkempty)
     except:
       checkcheck = "TRUE"
@@ -150,86 +174,104 @@ class BlacklistCMD(commands.Cog):
       for r in values:
         output = ', '.join(sheet.row_values(r.row))
         print(output)
-        A1, A2, A3, A4, A5, A6, A7, A8 ,A9, A10 = output.split(", ")
-        em.add_field(name = "Results: \n",value = "```python\n" + "Discord Username: " + str(A1) + "\nDiscord Tag Number: " + str(A2) + "\nDiscord ID: " + str(A3) + "\nGamertag: " + str(A4) +"\nBanned From: " + str(A5) + "\nKnown Alts: " + str(A6) + "\nBan Reason: " + str(A7) + "\nDate of Ban: " + str(A8) + "\nType of Ban: " + str(A9) + "\nDate the Ban Ends: " + str(A10) + "\n```",inline = False) 
+        A1, A2, A3, A4, A5, A6, A7, A8 ,A9 = output.split(", ")
+        em.add_field(name = "Results: \n",value = "```autohotkey\n" + "Discord Username: " + str(A1) + "\nDiscord ID: " + str(A2) + "\nGamertag: " + str(A3) +"\nBanned From: " + str(A4) + "\nKnown Alts: " + str(A5) + "\nBan Reason: " + str(A6) + "\nDate of Ban: " + str(A7) + "\nType of Ban: " + str(A8) + "\nDate the Ban Ends: " + str(A9) + "\n```",inline = False) 
       await ctx.send(embed = em)
     else:
       em.add_field(name = "No Results", value = "I'm sorry, but it looks like the [spreadsheet](https://docs.google.com/spreadsheets/d/1WKplLqk2Tbmy_PeDDtFV7sPs1xhIrySpX8inY7Z1wzY/edit?usp=sharing) doesn't have any results for the query you have sent! \n\n**Tips:**\n- Don't include the user's tag number! (Ex: #1879)\n- Try other ways of spelling out the username such as putting everything as lowercase! \n- Try checking the orginal spreadsheet for the value and try searching any term in the row here!")
       await ctx.send(embed = em)
    
-  
-
-  #searches gamertags
-  @commands.command()
-  @commands.has_role("Realm OP")
-  async def gsearch(self, ctx):
-    author = ctx.message.author
-    channel = ctx.message.channel
-    guild = ctx.message.guild
-    logfile = open("commandlog.txt", "a")
-    logfile.write(str(author.name) + " used GSEARCH \n")
-    logfile.close()
-    searchfile = open("Gamertags.txt", "r")
-    SearchResults = discord.Embed(title = "Search Results", description = "Requested by: " + author.mention, color = 0xb10d9f)
-    def check(m):
-      return m.content is not None and m.channel == channel and m.author is not self.bot.user
-    
-    await ctx.send("Specify a User or a Gamertag!")
-    message1 = await self.bot.wait_for('message', check=check)
-
-    message1c = message1.content 
-
-    if message1c.isdigit():   #DISCORD ID
-      for line in searchfile:
-        if message1c in line: 
-          #searchphrase being ID
-          Gamertag , ID = line.split(" ")
-          newI = Gamertag.replace("\n", "")
-          SearchResults.add_field(name = "Gamertag Results", value = "**Xbox Gamertag:** " + "**" + newI + "**" + "\n**Xbox Profile:** https://account.xbox.com/en-us/profile?gamertag=" + newI + "\n**Xbox Lookup:** https://xboxgamertag.com/search/" + newI)
-          SearchResults.set_thumbnail(url = guild.icon_url)
-          timestamp = datetime.now()
-          SearchResults.set_footer(text=guild.name + " | Date: " + str(timestamp.strftime(r"%x")))
-          await ctx.send(embed = SearchResults)
-          time.sleep(2)
-          return
-        else:
-          print()
-
-    else:
-      for line in searchfile:   #GAMERTAG
-        if message1c in line: 
-          #searchphrase being ID
-          Gamertag , ID = line.split(" ")
-          newI = ID.replace("\n", "")
-          GamertagI = Gamertag.replace("\n" , "")
-          SearchResults.add_field(name = "User Results", value = "Username: <@" + newI + "> \nThis user matched up with: **" + GamertagI + "**")
-          SearchResults.set_thumbnail(url = guild.icon_url)
-          timestamp = datetime.now()
-          SearchResults.set_footer(text=guild.name + " | Date: " + str(timestamp.strftime(r"%x")))
-          await ctx.send(embed = SearchResults)
-          time.sleep(2)
-          return
-
-        else:
-          await ctx.send(message1c + " was not found in the database!")
-          time.sleep(1)
-          newI = message1c
-          SearchResults.add_field(name = "Gamertag Search Failed", value = "**Xbox Gamertag:** " + "**" + newI + "**" + "\n**Xbox Profile:** https://account.xbox.com/en-us/profile?gamertag=" + newI + "\n**Xbox Lookup:** https://xboxgamertag.com/search/" + newI)
-          SearchResults.set_thumbnail(url = guild.icon_url)
-          timestamp = datetime.now()
-          SearchResults.set_footer(text=guild.name + " | Date: " + str(timestamp.strftime(r"%x")))
-          await ctx.send(embed = SearchResults)
-          time.sleep(2)
-          return
-
-  @gsearch.error
-  async def gsearc_error(self,ctx,error):
+  @bsearch.error
+  async def bsearch_error(self,ctx, error):
     if isinstance(error, commands.MissingRole):
-      await ctx.send("Uh oh, you can't run this! You aren't a Realm OP!")
+      await ctx.send("Uh oh, looks like you don't have the Realm OP role!")   
+    elif isinstance(error, commands.CommandInvokeError):
+      await ctx.send("Your search returned to many results. Please narrow your search, or try a different search term.") 
 
-      
+  @commands.command()
+  async def blogs(self, ctx):
+    author = ctx.message.author
+    def check(reaction, user):
+      return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
+    values = sheet.get_all_values()
+    #-----------
+    current_index = 2
+    results = values[current_index:current_index+1]
+    results =  ', '.join([str(elem) for elem in results])
+    print(results)
+    A1, A2, A3, A4, A5, A6, A7, A8 ,A9 = results.split(", ")
+    current_index+=1
+    results = values[current_index:current_index+1]
+    results =  ', '.join([str(elem) for elem in results])
+    B1, B2, B3, B4, B5, B6, B7, B8 ,B9 = results.split(", ")
+    current_index+=1
+    results = values[current_index:current_index+1]
+    results =  ', '.join([str(elem) for elem in results])
+    C1, C2, C3, C4, C5, C6, C7, C8 ,C9 = results.split(", ")
+    current_index+=1
+    #-----------
+    embed = discord.Embed(title = "MRP Blacklist Data", description = "Requested by Operator " + author.mention)
+    embed.add_field(name = "Spreadsheet", value = "```\n" + A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + "\n" + B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9 + "\n" + C1 + C2 + C3 + C4 + C5 + C6 + C7 + C8 + C9 + "\n```")
+    #Replace these 2 lines
+    #message = await ctx.send(values[current_index:current_index+3])
+    message = await ctx.send(embed = embed)
+    await message.add_reaction("◀️")
+    await message.add_reaction("▶️")
+    
+    while True:
+        try:
+            reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
+            # waiting for a reaction to be added - times out after x seconds, 60 in this
+            # example
 
+            if str(reaction.emoji) == "▶️":
+                results = values[current_index:current_index+1]
+                results =  ', '.join([str(elem) for elem in results])
+                A1, A2, A3, A4, A5, A6, A7, A8 ,A9 = results.split(", ")
+                current_index+=1
+                results = values[current_index:current_index+1]
+                results =  ', '.join([str(elem) for elem in results])
+                B1, B2, B3, B4, B5, B6, B7, B8 ,B9 = results.split(", ")
+                current_index+=1
+                results = values[current_index:current_index+1]
+                results =  ', '.join([str(elem) for elem in results])
+                C1, C2, C3, C4, C5, C6, C7, C8 ,C9 = results.split(", ")
+                current_index+=1
+                #embed.add_field(name = "Spreadsheet", value = "```\n" + A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + "\n" + B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9 + "\n" + C1 + C2 + C3 + C4 + C5 + C6 + C7 + C8 + C9 + "\n```")
+                newembed = discord.Embed(title = "MRP Blacklist Data", description = "Requested by Operator " + author.mention)
+                newembed.add_field(name = "Spreadsheet", value = "```\n" + A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + "\n" + B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9 + "\n" + C1 + C2 + C3 + C4 + C5 + C6 + C7 + C8 + C9 + "\n```")
+                await message.edit(embed = newembed)
+                await message.remove_reaction(reaction, user)
 
+            elif str(reaction.emoji) == "◀️":
+                results = values[current_index:current_index-1]
+                results =  ', '.join([str(elem) for elem in results])
+                print(results)
+                A1, A2, A3, A4, A5, A6, A7, A8 ,A9 = results.split(", ")
+                current_index-=1
+                results = values[current_index:current_index-1]
+                results =  ', '.join([str(elem) for elem in results])
+                B1, B2, B3, B4, B5, B6, B7, B8 ,B9 = results.split(", ")
+                current_index-=1
+                results = values[current_index:current_index-1]
+                results =  ', '.join([str(elem) for elem in results])
+                C1, C2, C3, C4, C5, C6, C7, C8 ,C9 = results.split(", ")
+                current_index-=1
+                #embed.add_field(name = "Spreadsheet", value = "```\n" + A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + "\n" + B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9 + "\n" + C1 + C2 + C3 + C4 + C5 + C6 + C7 + C8 + C9 + "\n```")
+                newembed = discord.Embed(title = "MRP Blacklist Data", description = "Requested by Operator " + author.mention)
+                newembed.add_field(name = "Spreadsheet", value = "```\n" + A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + "\n" + B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9 + "\n" + C1 + C2 + C3 + C4 + C5 + C6 + C7 + C8 + C9 + "\n```")
+                await message.edit(embed = newembed)
+                await message.remove_reaction(reaction, user)
+                                                         
+            else:
+                await message.remove_reaction(reaction, user)
+                # removes reactions if the user tries to go forward on the last page or
+                # backwards on the first page
+        except asyncio.TimeoutError:
+            await message.remove_reaction(reaction, user)
+            break
+            # ending the loop if user doesn't react after x seconds
 
+  
 def setup(bot):
   bot.add_cog(BlacklistCMD(bot))
