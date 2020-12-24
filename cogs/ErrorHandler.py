b01843bd2afa0399504f1d34067f02a0633b159d
InValidFire <invalidfire@mailbox.org>
2020-12-23 20:44:49 -0500

    Fix error handling

--- cogs/ErrorHandler.py
+++ cogs/ErrorHandler.py
@@ -44,13 +44,13 @@ class CommandErrorHandler(commands.Cog):
           with error_file.open("w") as f:
             f.write(exception_msg)
           with error_file.open("r") as f:
-            if dev_role not in ctx.user.roles:
+            if dev_role not in ctx.author.roles:
               await ctx.send(f"**Hey you!** *Mr. Turtle here has found an error, and boy is it a big one! I'll let the {dev_role.mention}'s know!*\nYou might also want to doublecheck what you sent and/or check out the help command!\nThe traceback file is attached below:", file=discord.File(f, "error.txt"))
             else:
               await ctx.send(f"**Hey guys look!** *A developer broke something big!* They should probably get to fixing that.\nThe traceback might be helpful though, good thing it's attached:", file=discord.File(f, "error.txt"))
             error_file.unlink()
         else:
-          if dev_role not in ctx.user.roles:
+          if dev_role not in ctx.author.roles:
             await ctx.send(f"**Hey you!** *Mr. Turtle here has found an error! I'll let the {dev_role.mention}'s know!*\nYou might also want to doublecheck what you sent and/or check out the help command!\n**Error:** ```\n{exception_msg}\n```")
           else:
             await ctx.send(f"**Hey guys look!** *A developer broke something!* They should probably get to fixing that.\nThe traceback could be useful: ```\n{exception_msg}\n```")
