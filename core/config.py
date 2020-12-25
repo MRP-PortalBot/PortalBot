b3e4fe573549b62272d9a919017aaf5c1340975b
InValidFire <invalidfire@mailbox.org>
2020-12-24 23:16:52 -0500

    Documentation

--- core/config.py
+++ core/config.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+from typing import Tuple
 import json
-def load_config():
+
+def load_config() -> Tuple[dict, Path]:
+  """Load data from the botconfig.json.\n
+  Returns a tuple containing the data as a dict, and the file as a Path"""
   config_file = Path("botconfig.json")
   config_file.touch(exist_ok=True)
   if config_file.read_text() == "":
@@ -10,6 +14,7 @@ def load_config():
   return config, config_file
 
 def prompt_config(msg, key):
+  """Ensure a value exists in the botconfig.json, if it doesn't prompt the bot owner to input via the console."""
   config, config_file = load_config()
   if key not in config:
     config[key] = input(msg)
