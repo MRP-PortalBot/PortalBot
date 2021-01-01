bf365d9f72ab9fa941f2fbda0993ba5ce41dd902
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2020-12-31 23:38:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -59,8 +59,6 @@ def get_extensions():  # Gets extension list dynamically
 
 @client.event
 async def on_ready():
-    print(f"{bcolors.WARNING}Attempting to connect to Discord API...{bcolors.ENDC}")
-    await asyncio.sleep(1)
     print(f"{bcolors.OKGREEN}Successfully connected to Discord!{bcolors.ENDC}")
     print(f"{bcolors.OKCYAN}BOT INFORMATION: {bcolors.ENDC}")
     print(f"{bcolors.OKCYAN}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>{bcolors.ENDC}")
