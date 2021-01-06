f19594e4a4f57ea466eefeed22f35cfe1beaad79
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-05 19:10:20 -0500

    Update RealmCMD.py

--- cogs/RealmCMD.py
+++ cogs/RealmCMD.py
@@ -55,7 +55,8 @@ class RealmCMD(commands.Cog):
         #category = discord.utils.get(guild.categories, name = "Realm Channels List Test")
         category = discord.utils.get(guild.categories, name="🎮 Realms & Servers")
         channel = await category.create_text_channel(realm + "-" + emoji)
-        await channel.send(role.mention + " **Welcome to the MRP!** \n Your channel has been created and you should have gotten a DM regarding some stuff about your channel! \n If you have any questions, feel free to DM an Admin or a Moderator! ")
+        welcomeEM = discord.Embed(title = "Welcome to the MRP!", description = f"{role.mention} **Welcome to the MRP!** \n Your channel has been created and you should have gotten a DM regarding some stuff about your channel! \n If you have any questions, feel free to DM an Admin or a Moderator!")
+        await channel.send(embed = welcomeEM)
         await channel.edit(topic="The newest Realm on the Minecraft Realm Portal, Check it out and chat with the owners for more Realm information. \n \n ]]Realm: Survival Multiplayer[[")
         ChannelCreate = "DONE"
         await user.add_roles(role)
@@ -64,38 +65,38 @@ class RealmCMD(commands.Cog):
         perms.manage_channels = True
         perms.manage_webhooks = True
         perms.manage_messages = True
+
+        Muted = discord.utils.get(ctx.guild.roles, name="muted")
+        permsM = channel.overwrites_for(Muted)
+        permsM.read_messages = False
+        permsM.send_messages = False
         await channel.set_permissions(role, overwrite=perms, reason="Created New Realm!")
 
         # This try statement is here incase we are testing this in the testing server as this channel does not appear in that server!
-        try:
+        if ctx.guild.id == 587495640502763521:
             channelrr = guild.get_channel(683454087206928435)
+            print(channelrr)
             await channelrr.send(role.mention + "\n **Please agree to the rules to gain access to the Realm Owner Chats!**")
             perms12 = channelrr.overwrites_for(role)
             perms12.read_messages = True
+        ChannelPermissions = "DONE"
+        await channel.set_permissions(Muted, overwrite=permsM)
+        DMStatus = "FAILED"
+        embed = discord.Embed(title="Congrats On Your New Realm Channel!",description="Your new channel: <#" + str(channel.id) + ">", color=0x42f5bc)
+        embed.add_field(name="Information", value="Enjoy your new channel. Use this channel to advertise your realm, and engage the community. The more active a channel the more likely people will be to stop by and check you out. You have moderation privileges in your channel. You can change the description, pin messages, and delete messages. You now have access to the Realm Owner Chats. Before they will be fully unlocked you will need to agree to the rules in #realm-op-rules. If you would like to add an OP to your team, in your channel type: \n```>addOP @newOP @reamlrole``` \n", inline = True)
+        embed.add_field(name="Realm Information Embed",value="In order to have your Realm listed in #realm-channels-info, please do not remove the ]]Realm: Survival Multiplayer[[ portion of your channel description. Feel free to edit this in the following way ]]Anything You Want To Show Up After Your Realm Name: Short Description Of Your Realm[[. ", inline = True)
+        embed.add_field(name="Questions", value="Thanks for joining the Portal, and if you have any questions contact an Admin or a Moderator!", inline = True)
+        embed.set_thumbnail(url = user.avatar_url)
+        try:
+            await user.send(embed=embed)
+            DMStatus = "DONE"            
+
         finally:
-            Muted = discord.utils.get(ctx.guild.roles, name="Muted")
-            permsM = channel.overwrites_for(Muted)
-            permsM.read_messages = False
-            permsM.send_messages = False
-            ChannelPermissions = "DONE"
-            await channel.set_permissions(Muted, overwrite=permsM)
-            try:
-                embed = discord.Embed(title="Congrats On Your New Realm Channel!",description="Your new channel: <#" + str(channel.id) + ">", color=0x42f5bc)
-                embed.add_field(name="Information", value="Enjoy your new channel. Use this channel to advertise your realm, and engage the community. The more active a channel the more likely people will be to stop by and check you out. You have moderation privileges in your channel. You can change the description, pin messages, and delete messages. You now have access to the Realm Owner Chats. Before they will be fully unlocked you will need to agree to the rules in #realm-op-rules. If you would like to add an OP to your team, in your channel type: \n```>addOP @newOP @reamlrole``` \n", inline = True)
-                embed.add_field(name="Realm Information Embed",value="In order to have your Realm listed in #realm-channels-info, please do not remove the ]]Realm: Survival Multiplayer[[ portion of your channel description. Feel free to edit this in the following way ]]Anything You Want To Show Up After Your Realm Name: Short Description Of Your Realm[[. ", inline = True)
-                embed.add_field(name="Questions", value="Thanks for joining the Portal, and if you have any questions contact an Admin or a Moderator!", inline = True)
-                await user.send(embed=embed)
-            except:
-                await ctx.send("Uh oh, something went wrong while trying to DM the Realm Owner. \n`Error: Discord Forbidden (User's Privacy Settings Prevented the DM Message)`")
-                await ctx.send("DM Status: **FAILED**")
-                DMStatus = "FAILED"
-            else:
-                DMStatus = "DONE"
-            finally:
-                embed = discord.Embed(title="Realm Channel Output", description="Realm Requested by: " + author.mention, color=0x38ebeb)
-                embed.add_field(name="**Console Logs**", value="**Role Created:** " + RoleCreate + " -> " + role.mention + "\n**Channel Created:** " + ChannelCreate +" -> <#" + str(channel.id) + ">\n**Role Given:** " + RoleGiven + "\n**Channel Permissions:** " + ChannelPermissions + "\n**DMStatus:** " + DMStatus)
-                embed.set_footer(text = "The command has finished all of its tasks")
-                await ctx.send(embed=embed)
+            embed = discord.Embed(title="Realm Channel Output", description="Realm Requested by: " + author.mention, color=0x38ebeb)
+            embed.add_field(name="**Console Logs**", value="**Role Created:** " + RoleCreate + " -> " + role.mention + "\n**Channel Created:** " + ChannelCreate +" -> <#" + str(channel.id) + ">\n**Role Given:** " + RoleGiven + "\n**Channel Permissions:** " + ChannelPermissions + "\n**DMStatus:** " + DMStatus)
+            embed.set_footer(text = "The command has finished all of its tasks")
+            embed.set_thumbnail(url = user.avatar_url)
+            await ctx.send(embed=embed)
 
     @newrealm.error
     async def newrealm_error(self, ctx, error):
