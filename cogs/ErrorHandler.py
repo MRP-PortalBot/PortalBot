59e1f3cfcba3ae1ea344bf450534ba626ee943cf
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-08 20:17:02 -0500

    Gist URL Changes

--- cogs/ErrorHandler.py
+++ cogs/ErrorHandler.py
@@ -8,6 +8,7 @@ import asyncio
 import requests
 import yarl
 import os
+import json
 
 
 class GithubError(commands.CommandError):
@@ -27,72 +28,6 @@ class CommandErrorHandler(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    async def github_request(self, method, url, *, params=None, data=None, headers=None):
-        hdrs = {
-            'Accept': 'application/vnd.github.inertia-preview+json',
-            'User-Agent': 'RoboDanny DPYExclusive Cog',
-            'Authorization': f'token {os.getenv("GIST")}'
-        }
-
-        req_url = yarl.URL('https://api.github.com') / url
-
-        if headers is not None and isinstance(headers, dict):
-            hdrs.update(headers)
-
-
-        async with self.bot.session.request(method, req_url, params=params, json=data, headers=hdrs) as r:
-            remaining = r.headers.get('X-Ratelimit-Remaining')
-            js = await r.json()
-            if r.status == 429 or remaining == '0':
-                # wait before we release the lock
-                delta = discord.utils._parse_ratelimit_header(r)
-                await asyncio.sleep(delta)
-                self._req_lock.release()
-                return await self.github_request(method, url, params=params, data=data, headers=headers)
-            elif 300 > r.status >= 200:
-                return js
-            else:
-                raise GithubError(js['message'])
-
-
-    async def create_gist(self, content, *, description=None, filename=None, public=True):
-        headers = {
-            'Accept': 'application/vnd.github.v3+json',
-        }
-
-        filename = filename or 'output.txt'
-        data = {
-            'public': public,
-            'files': {
-                filename: {
-                    'content': content
-                }
-            }
-        }
-
-        description = 'PortalBot Traceback'
-
-        js = await self.github_request('POST', 'gists', data=data, headers=headers)
-        return js['html_url']
-
-    async def redirect_attachments(self, message):
-        attachment = message.attachments[0]
-        if not attachment.filename.endswith(('.txt', '.py', '.json')):
-            return
-
-        # If this file is more than 2MiB then it's definitely too big
-        if attachment.size > (2 * 1024 * 1024):
-            return
-
-        try:
-            contents = await attachment.read()
-            contents = contents.decode('utf-8')
-        except (UnicodeDecodeError, discord.HTTPException):
-            return
-
-        gist = await self.create_gist(contents, description = 'PortalBot Traceback', filename=attachment.filename)
-        await message.channel.send(f'File automatically uploaded to gist: <{gist}>')
 
     @commands.command()
     async def error(self, ctx, times: int = 20, msg="error"):
@@ -122,6 +57,19 @@ class CommandErrorHandler(commands.Cog):
                 with error_file.open("w") as f:
                     f.write(exception_msg)
                 with error_file.open("r") as f:
+                    data = "\n".join([l.strip() for l in f])
+
+                    GITHUB_API="https://api.github.com"
+                    API_TOKEN=os.getenv("GIST")
+                    url=GITHUB_API+"/gists"
+                    print(f"Request URL: {url}")                    
+                    headers={'Authorization':'token %s'%API_TOKEN}
+                    params={'scope':'gist'}
+                    payload={"description":"GIST created by python code","public":True,"files":{"error":{"content": f"{data}"}}}
+                    res=requests.post(url,headers=headers,params=params,data=json.dumps(payload))
+                    j=json.loads(res.text)
+                    gisturl = "gist.github.com/%s"%(j['id'])
+
                     if dev_role not in ctx.author.roles:
                         embed = discord.Embed(title = "Traceback Detected!", description = f"**Hey you!** *Mr. Turtle here has found an error, and boy is it a big one! I'll let the {dev_role.mention}'s know!*\nYou might also want to doublecheck what you sent and/or check out the help command!", color = 0xfc3d03)
                         embed.add_field(name = "Bug Reporting", value = "Have any extra information that could help resolve this issue? Feel free to use the **>bug** command! \nUsage: `>bug (extra valuable information here!)` \n\nExamples of helpful information! \n\n- Any arguments you provided with the command\n- What the actual problem was (What went wrong?)\n- Any other information that could help!")
@@ -129,10 +77,11 @@ class CommandErrorHandler(commands.Cog):
                         await ctx.send(embed = embed)
                         guild = self.bot.get_guild(448488274562908170)
                         channel = guild.get_channel(797193549992165456)
-                        embed2 = discord.Embed(title = "Traceback Detected!", description = f"**Information:**\n**Server:** {ctx.message.guild.name}\n**User:** {ctx.message.author.mention}\n**Traceback:** *Download Below*", color= 0xfc3d03)
+                        embed2 = discord.Embed(title = "Traceback Detected!", description = f"**Information:**\n**Server:** {ctx.message.guild.name}\n**User:** {ctx.message.author.mention}", color= 0xfc3d03)
+                        embed2.add_field(name = "Gist URL", value = [gisturl]("Uploaded Traceback to GIST"))
                         await channel.send(embed = embed2)
-                        message = await channel.send(file=discord.File(f, "error.txt"))
-                        await self.redirect_attachments(message)
+               
+           
 
 
                     else:
