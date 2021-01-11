883dc7262af05229fcc22fa7754a0abf7758b5da
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-11 08:45:17 -0500

    Update Events.py

--- cogs/Events.py
+++ cogs/Events.py
@@ -13,11 +13,9 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if member.guild.id == 192052103017922567:
-            return
-        elif member.guild.id == 587495640502763521:
+        if member.guild.id == 587495640502763521:
             guild = self.bot.get_guild(587495640502763521)
-            channel = guild.get_channel(791066359013441546)
+            channel = guild.get_channel(588813558486269956)
             count = int(member.guild.member_count) + 1
             embed = discord.Embed(title = f"Welcome to the {member.guild.name}", description = f"**{str(member.display_name)}** is the **{str(count)}**th member!", color = 0xb10d9f)
             embed.set_thumbnail(url=member.avatar_url)
@@ -25,7 +23,6 @@ class Events(commands.Cog):
             await channel.send(embed=embed)
         else:
             print(f"Unhandled Server: {member.display_name} | {member.guild.name}")
-            return
 
 
 def setup(bot):
