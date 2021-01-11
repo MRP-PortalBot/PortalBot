52a8d7571dae0dbd6ceefa2cc665d421242f0b12
BSavage81 <beau.j.savage@gmail.com>
2021-01-11 12:25:18 -0600

    test on join

--- cogs/Events.py
+++ cogs/Events.py
@@ -7,6 +7,34 @@ import asyncio
 from discord import Embed
 import requests
 
+# --------------------------------------------------
+# pip3 install gspread oauth2client
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+gtsheet = client.open("PortalbotProfile").sheet1
+sheet = client.open("MRP Blacklist Data").sheet1
+# 3 Values to fill
+
+# Template on modfying spreadsheet
+'''
+gtrow = ["1", "2", "3"]
+gtsheet.insert_row(row, 3)
+print("Done.")
+
+gtcell = sheet.cell(3,1).value
+print(cell)
+'''
+# -----------------------------------------------------
+
 class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -23,6 +51,11 @@ class Events(commands.Cog):
             await channel.send(embed=embed)
         else:
             print(f"Unhandled Server: {member.display_name} | {member.guild.name}")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = guild.get_channel(791066359013441546)
+        await channel.send("This Worked")
             
 
 
