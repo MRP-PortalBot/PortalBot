17bc553794a9e08eefb87a48af6ab1dfb2c6c31d
BSavage81 <bsavage81.aurafall@gmail.com>
2021-01-14 12:02:46 -0600

    fixed stupid footer

--- cogs/MGPonlyCMD.py
+++ cogs/MGPonlyCMD.py
@@ -61,7 +61,7 @@ class MGPonlyCMD(commands.Cog):
             title="Player's are being called by", description=author.mention, color=0xFFCE41)
 
         callembed.add_field(name=authorname + "wants to play" + gamename, value="Is there anyone @here that would like to join?", inline=True)
-        callembed.add_footer(text="This command cannot be used again for 1 hour!")
+        callembed.set_footer(text="This command cannot be used again for 1 hour!")
 
         await channel.send("success")
         await channel.send(embed=callembed)
