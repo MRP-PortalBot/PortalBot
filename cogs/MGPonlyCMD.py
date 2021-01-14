509da42a6255209ab2665e41907e29f87dbfc94d
BSavage81 <bsavage81.aurafall@gmail.com>
2021-01-14 11:28:36 -0600

    added summon command

---
+++ cogs/MGPonlyCMD.py
@@ -0,0 +1,72 @@
+from logging import exception
+import discord
+from discord.ext import commands
+import time
+from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import asyncio
+from core.common import load_config
+config, _ = load_config()
+i = 1
+time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+
+# -------------------------------------------------------
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open(
+    "Minecraft Realm Portal Channel Application (Responses)").sheet1
+
+sheet2 = client.open("MRPCommunityRealmApp").sheet1
+# -------------------------------------------------------
+
+def check_MRP():
+    def predicate(ctx):
+        return ctx.message.guild.id == 587495640502763521 or ctx.message.guild.id == 448488274562908170
+    return commands.check(predicate)
+
+
+def check_MGP():
+    def predicate(ctx):
+        return ctx.message.guild.id == 192052103017922567 or ctx.message.guild.id == 448488274562908170
+    return commands.check(predicate)
+
+def convert(time):
+    try:
+        return int(time[:-1]) * time_convert[time[-1]]
+    except:
+        return time
+
+
+class MGPonlyCMD(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @check_MGP()
+    @commands.cooldown(1, 3600, commands.BucketType.channel)
+    async def summon(self, ctx):
+        author = ctx.message.author
+        channel = ctx.message.channel
+        authorname = author.name
+        gamename = channel.name
+        embed = discord.Embed(
+            title="Player's are being Summoned by", description=author.mention, color=0xFFCE41)
+        embed.add_field(name=authorname + "wants to play" + gamename, value="Is anyone @here that would like to join?", inline=True)
+        embed.add_footer(text="This command cannot be used again for 1 hour!")
+
+        await ctx.send(embed=embed)
+
+    @summon.error
+    async def summon_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send("This command cannot be used again for 1 hour!")
+
+def setup(bot):
+    bot.add_cog(MGPonlyCMD(bot))
