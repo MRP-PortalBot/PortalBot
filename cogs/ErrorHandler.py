150ef880bbd88b5ad0897bab00a8bc2e19b07367
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-14 10:23:45 -0500

    Merge Commands and add query as a global function

--- cogs/ErrorHandler.py
+++ cogs/ErrorHandler.py
@@ -9,24 +9,7 @@ import requests
 import yarl
 import os
 import json
-
-url = "https://api.trello.com/1/cards"
-
-def query(authorname, ID, server, channel, suggestion):
-    query = {
-        'key': os.getenv("TRELLOKEY"),
-        'token': os.getenv("TRELLOTOKEN"),
-        'idList': '5fff8cd40de14a1cdc6fd79a',
-        'pos': 'top',
-        'name': f'[BUG] by {authorname}',
-        'desc': f'Author ID: {ID}\nGuild: {server}\nChannel: {channel}\n\nSuggestion: {suggestion}'
-    }
-    response = requests.request(
-        "POST",
-        url,
-        params=query
-    )
-    return response.text
+from core.common import query
 
 
 class GithubError(commands.CommandError):
@@ -127,22 +110,58 @@ class CommandErrorHandler(commands.Cog):
         raise error
 
     @commands.command()
-    async def bug(self, ctx, *, bug : str):
-        author = ctx.message.author
-        channel = ctx.message.channel
-        responseguild = ctx.message.guild
+    async def report(self, ctx, typer, *, feedback : str):
+        def check(m):
+            return m.content is not None and m.channel == channel and m.author is not self.bot.user and m.author == author
+        def check2(reaction, user):
+            return user == ctx.author and (str(reaction.emoji) == '✅' or str(reaction.emoji) == '❌')
+        if typer == "bug" or typer == "Bug" or typer == "BUG":
+            author = ctx.message.author
+            channel = ctx.message.channel
+            responseguild = ctx.message.guild
 
-        query(author.name, author.id, responseguild, channel.name, bug)
-        guild = self.bot.get_guild(448488274562908170)
-        channel = guild.get_channel(797193549992165456)
-        embed = discord.Embed(title = "User Bug Report!", description = f"Author: {author.mention}\nChannel: {channel.name}\nServer: {responseguild.name}", color=0xfc8003)
-        embed.add_field(name = "Feedback", value = "[Trello URL](https://trello.com/b/kSjptEEb/portalbot-dev-trello)")
-        await channel.send(embed = embed)
-        resp = discord.Embed(title = "Thank You For Submitting A Bug Report!", description = "I have successfully sent in your bug report!", color= 0xfc8003)
-        resp.add_field(name = "Feedback Sent:", value = "[Trello URL](https://trello.com/b/kSjptEEb/portalbot-dev-trello)")
-        await ctx.send(embed=resp)
+            query(author.name, author.id, responseguild, channel.name, feedback, "BUG")
+            guild = self.bot.get_guild(448488274562908170)
+            channel = guild.get_channel(797193549992165456)
+            embed = discord.Embed(title = "User Bug Report!", description = f"Author: {author.mention}\nChannel: {channel.name}\nServer: {responseguild.name}", color=0xfc8003)
+            embed.add_field(name = "Feedback", value = "[Trello URL](https://trello.com/b/kSjptEEb/portalbot-dev-trello)")
+            await channel.send(embed = embed)
+            resp = discord.Embed(title = "Thank You For Submitting A Bug Report!", description = "I have successfully sent in your bug report!", color= 0xfc8003)
+            resp.add_field(name = "Feedback Sent:", value = "[Trello URL](https://trello.com/b/kSjptEEb/portalbot-dev-trello)")
+            await ctx.send(embed=resp)
+        elif typer == "suggestion" or typer == "Suggestion" or typer == "SUGGESTION":
+            author = ctx.message.author
+            authorname = ctx.message.author.display_name
+            ID = ctx.message.author.id
+            server = ctx.message.guild.name
+            channel = ctx.message.channel
+            await channel.send("Suggestion/Feedback")
+            suggestion = await self.bot.wait_for('message', check=check)
 
+            embed = discord.Embed(title = "Ready to Submit?", description = "Before you submit!\nPlease make sure that the following response is **not** a BUG REPORT! Bug Reports should be filled using the `bug` command! *(Use the bug tag for more information!)* ", color = 0x4c594b)
+            embed.add_field(name = "Submit Feedback", value = "✅ - SUBMIT\n❌ - CANCEL")
+            message = await ctx.send(embed = embed)
+            reactions = ['✅', '❌']
+            for emoji in reactions:
+                await message.add_reaction(emoji)
 
+            def check2(reaction, user):
+                return user == ctx.author and (str(reaction.emoji) == '✅' or str(reaction.emoji) == '❌')
+
+            try:
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=100.0, check=check2)
+                if str(reaction.emoji) == "❌":
+                    await channel.send("Ended Task")
+                    await message.delete()
+                    return
+                else:
+                    await message.delete()
+                    query(authorname, ID, server, channel.name, suggestion.content, "Suggestion")
+                    embed = discord.Embed(title = "I have sent in your suggestion!", description = f"You can view your suggestion's progress here! [Trello URL](https://trello.com/b/kSjptEEb/portalbot-dev-trello)", color = 0x4c594b)
+                    await ctx.send(embed = embed)
+
+            except asyncio.TimeoutError:
+                await channel.send("Looks like you didn't react in time, please try again later!")
 
 def setup(bot):
     bot.add_cog(CommandErrorHandler(bot))
