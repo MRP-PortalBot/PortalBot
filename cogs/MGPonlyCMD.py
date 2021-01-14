2cc01b6e6350128f1326fcf79330143221b27fb3
BSavage81 <bsavage81.aurafall@gmail.com>
2021-01-14 11:41:27 -0600

    update

--- cogs/MGPonlyCMD.py
+++ cogs/MGPonlyCMD.py
@@ -50,18 +50,19 @@ class MGPonlyCMD(commands.Cog):
 
     @commands.command()
     @check_MGP()
-    @commands.cooldown(1, 3600, commands.BucketType.channel)
+    @commands.cooldown(1, 30, commands.BucketType.channel)
     async def gametime(self, ctx):
         author = ctx.message.author
         channel = ctx.message.channel
         authorname = author.name
         gamename = channel.name
-        embed = discord.Embed(
+        callembed = discord.Embed(
             title="Player's are being called by", description=author.mention, color=0xFFCE41)
-        embed.add_field(name=authorname + "wants to play" + gamename, value="Is there anyone @here that would like to join?", inline=True)
-        embed.add_footer(text="This command cannot be used again for 1 hour!")
 
-        await ctx.send(embed=embed)
+        callembed.add_field(name=authorname + "wants to play" + gamename, value="Is there anyone @here that would like to join?", inline=True)
+        callembed.add_footer(text="This command cannot be used again for 1 hour!")
+
+        await channel.send(embed=callembed)
 
     @gametime.error
     async def gametime_error(self, ctx, error):
