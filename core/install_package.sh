9038dadd32389ed1c0c5d23c5b0d8871dc967eaa
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-19 19:58:23 -0500

    Create install_package.sh

---
+++ core/install_package.sh
@@ -0,0 +1,9 @@
+pip install Pillow
+pip install PyGithub
+pip install discord-sentry-reporting
+pip install gspread
+pip install xbox
+
+echo "Packages have been downlaoded, returning to file!"
+
+python3 main.py
