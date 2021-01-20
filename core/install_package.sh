5cd9ca8924deae7040b515c397e6e142b4209931
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-19 20:08:26 -0500

    Update install_package.sh

--- core/install_package.sh
+++ core/install_package.sh
@@ -5,7 +5,7 @@ pip install gspread
 pip install xbox
 pip install discord-py-slash-command
 pip install --upgrade sentry-sdk
-
+python3 -m pip install --upgrade pip
 
 echo "Packages have been downlaoded, returning to file!"
 
