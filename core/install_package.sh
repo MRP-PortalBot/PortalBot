568847b3549f1b3ddfce25b93a004ada6cbbee94
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-19 20:01:43 -0500

    Update install_package.sh

--- core/install_package.sh
+++ core/install_package.sh
@@ -3,6 +3,10 @@ pip install PyGithub
 pip install discord-sentry-reporting
 pip install gspread
 pip install xbox
+pip install discord-py-slash-command
+pip install --upgrade sentry-sdk
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Packages have been downlaoded, returning to file!"
 
