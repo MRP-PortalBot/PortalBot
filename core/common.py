58ebd00ba80ad5499fa9ea28818955aa9a203cc2
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-01-20 19:11:41 -0500

    return fix

--- core/common.py
+++ core/common.py
@@ -31,7 +31,7 @@ async def paginate_embed(bot: discord.Client, ctx, embed: discord.Embed, populat
     emotes = ["◀️", "▶️"]
 
     async def check_reaction(reaction, user):
-        return user == ctx.author and str(reaction.emoji) in emotes
+         return await user == ctx.author and str(reaction.emoji) in emotes
 
     embed = await population_func(embed, page)
     if isinstance(embed, discord.Embed):
