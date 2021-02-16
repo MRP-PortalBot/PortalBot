4d99e68e3941ba950cc80692032d51b4365725a6
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-02-15 20:34:00 -0500

    Formatting

--- cogs/MRP-Cogs/JiraReports.py
+++ cogs/MRP-Cogs/JiraReports.py
@@ -29,18 +29,21 @@ class SkeletonCMD(commands.Cog):
                     #await ctx.send("No results!")
                     #return
                 e = discord.Embed(title = f"ISSUE: {issue.key}", description = f"Issue Title: {issue.fields.summary}", color = 0x42f5e9)
-                e.add_field(name = "Basic Information:", value = f"```autohotkey\nIssue Reporter:{issue.fields.reporter.name} State: {issue.fields.status}\nIssue State: {issue.fields.customfield_10500.value}\nConfirmation Status: {issue.fields.issuetype}\nCreated At: {m}/{d}/{y}\nSummery: {issue.fields.issuetype.description}")
-                e.add_field(name = f"Details:" ,value = f"Description: {issue.fields.description}```\n**ISSUE LINK:** {issue.permalink()}")
+                e.add_field(name = "Basic Information:", value = f"```autohotkey\nIssue Reporter:{issue.fields.reporter.name} State: {issue.fields.status}\nIssue State: {issue.fields.customfield_10500.value}\nConfirmation Status: {issue.fields.issuetype}\nCreated At: {m}/{d}/{y}\nSummery: {issue.fields.issuetype.description}```")
+                e.add_field(name = f"Details:" ,value = f"```autohotkey\nDescription: {issue.fields.description}\n**ISSUE LINK:** {issue.permalink()}```", inline = False)
                 await ctx.send(embed = e)
 
         else:
             for issue in jira.search_issues(f'text = "{query}"'):
+                x = issue.fields.created
+                x, nonehere = x.split("T")
+                y, m, d = x.split("-")
                 #if issue == None or issue == "" or issue == " ": 
                     #await ctx.send("No results!")
                     #return
                 e = discord.Embed(title = f"ISSUE: {issue.key}", description = f"Issue Title: {issue.fields.summary}", color = 0x42f5e9)
-                e.add_field(name = "Basic Information:", value = f"```autohotkey\nIssue Reporter:{issue.fields.reporter.name} State: {issue.fields.status}\nIssue State: {issue.fields.customfield_10500.value}\nConfirmation Status: {issue.fields.issuetype}\nCreated At: {m}/{d}/{y}\nSummery: {issue.fields.issuetype.description}")
-                e.add_field(name = f"Details:" ,value = f"Description: {issue.fields.description}```\n**ISSUE LINK:** {issue.permalink()}")
+                e.add_field(name = "Basic Information:", value = f"```autohotkey\nIssue Reporter:{issue.fields.reporter.name} State: {issue.fields.status}\nIssue State: {issue.fields.customfield_10500.value}\nConfirmation Status: {issue.fields.issuetype}\nCreated At: {m}/{d}/{y}\nSummery: {issue.fields.issuetype.description}```")
+                e.add_field(name = f"Details:" ,value = f"```autohotkey\nDescription: {issue.fields.description}\n**ISSUE LINK:** {issue.permalink()}```", inline = False)
                 await ctx.send(embed = e)
 
 
