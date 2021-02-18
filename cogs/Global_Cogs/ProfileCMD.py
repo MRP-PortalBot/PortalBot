4151cc0604615c5d86343642788fd18c0cba3ddb
BSavage81 <beau.j.savage@gmail.com>
2021-02-18 09:53:36 -0600

    update profile

--- cogs/Global_Cogs/ProfileCMD.py
+++ cogs/Global_Cogs/ProfileCMD.py
@@ -127,14 +127,13 @@ class ProfileCMD(commands.Cog):
                     else:
                         profileembed.set_footer(text="Requested by " + author.name)
                     if role in author.roles: 
-                        for data in blacklistdata:
-                            qID = (database.MRP_Blacklist_Data.select().where(data.contains(longid)))
-                            qNAME = (database.MRP_Blacklist_Data.select().where(data.contains(discordname)))
-                            if qID.exists() or qNAME.exists():
-                                profileembed.add_field(name="BANNED PLAYER", value="Player is on the banned players list", inline=False)
-                                await ctx.send(embed=profileembed)
-                            else:
-                                await ctx.send(embed=profileembed)
+                        qID = (database.MRP_Blacklist_Data.select().where(blacklistdata.contains(longid)))
+                        qNAME = (database.MRP_Blacklist_Data.select().where(blacklistdata.contains(discordname)))
+                        if qID.exists() or qNAME.exists():
+                            profileembed.add_field(name="BANNED PLAYER", value="Player is on the banned players list", inline=False)
+                            await ctx.send(embed=profileembed)
+                        else:
+                            await ctx.send(embed=profileembed)
                     else:
                         await ctx.send(embed=profileembed)
 
