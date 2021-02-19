74cb488f8bc9b7c3befad3feaa7c685670f007e6
Space-Turtle0 <56415283+Space-Turtle0@users.noreply.github.com>
2021-02-18 19:38:48 -0500

    Update install_package.sh

--- core/install_package.sh
+++ core/install_package.sh
@@ -7,6 +7,7 @@ pip install gspread
 pip install xbox
 pip install discord-py-slash-command
 pip install --upgrade sentry-sdk
+pip install pymysql
 
 echo -e "\n\e[33mPackages have been downlaoded, returning to file!\n"
 
