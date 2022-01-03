f9a120944b82b714006609cb4e318c3c168a5e28
BSavage81 <beau.j.savage@gmail.com>
2022-01-03 15:02:15 -0600

    test8

--- cogs/Global_Cogs/ProfileCMD.py
+++ cogs/Global_Cogs/ProfileCMD.py
@@ -112,17 +112,17 @@ class ProfileCMD(commands.Cog):
                 profileembed.set_thumbnail(url=pfp)
                 profileembed.add_field(name="Discord", value=discordname, inline=True)
                 profileembed.add_field(name="LongID", value=longid, inline=True)
-                if tzone != "Null":
+                if tzone.is_null(False):
                     profileembed.add_field(name="Timezone", value=tzone, inline=True)
-                if xbox != "Null":
+                if xbox.is_null(False):
                     profileembed.add_field(name="XBOX Gamertag", value=xbox, inline=False)
-                if psnid != "Null":
+                if psnid.is_null(False):
                     profileembed.add_field(name="Playstation ID", value=psnid, inline=False) 
-                if switch != "Null":
+                if switch.is_null(False):
                     profileembed.add_field(name="Switch Friend Code", value=switch, inline=False) 
-                if pokemongo != "Null":
+                if pokemongo.is_null(False):
                     profileembed.add_field(name="Pokemon Go ID", value=pokemongo, inline=False) 
-                if chessdotcom != "Null":
+                if chessdotcom.is_null(False):
                     profileembed.add_field(name="Chess.com ID", value=chessdotcom, inline=False)      
                 if username == ctx.message.author:
                     profileembed.set_footer(text="If you want to edit your profile, use the command >profile edit")
