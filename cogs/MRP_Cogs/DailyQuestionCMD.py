e3e7d01988beb882f209d528fde241bc67ee55d5
BSavage81 <beau.j.savage@gmail.com>
2022-02-18 13:00:17 -0600

    Update DailyQuestionCMD.py

--- cogs/MRP_Cogs/DailyQuestionCMD.py
+++ cogs/MRP_Cogs/DailyQuestionCMD.py
@@ -214,7 +214,7 @@ class DailyCMD(commands.Cog):
 
 
     
-    @slash_command(name="repeatq", description = "Repeat a daily question by id number", guild_ids=[config['SlashServer1'],config['SlashServer2'],config['SlashServer3']], permissions = "manage_roles")
+    @slash_command(name="repeatq", description = "Repeat a daily question by id number", guild_ids=[config['SlashServer1'],config['SlashServer2'],config['SlashServer3']], permissions=["manage_roles"])
     async def repeatq(self, ctx, number):
         """Activate a question"""
         q: database.Question = database.Question.select().where(database.Question.id == number).get()
