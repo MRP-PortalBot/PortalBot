ee0772b58a480e45ce2f313a8753d657c038f97e
BSavage81 <beau.j.savage@gmail.com>
2024-09-23 09:16:59 -0500

    Update profiles.py

--- utils/global_cogs/profiles.py
+++ utils/global_cogs/profiles.py
@@ -18,7 +18,7 @@ class ProfileCMD(commands.Cog):
 
     BAR_HEIGHT = 30  # Progress bar height
     RADIUS = 15  # Rounded corners radius for the progress bar
-    FONT_PATH = "./core/fonts/GAMERIA2.ttf"
+    FONT_PATH = "./core/fonts/monocraft.ttf"
     BACKGROUND_IMAGE_PATH = './core/images/profilebackground4.png'
     TEXT_COLOR = (255, 255, 255, 255)
     SHADOW_COLOR = (0, 0, 0, 200)  # Black with transparency
