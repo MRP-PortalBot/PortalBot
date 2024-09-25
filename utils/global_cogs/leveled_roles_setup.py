a1bd443304938044a7226fa3d33ac7b2eb832de0
BSavage81 <beau.j.savage@gmail.com>
2024-09-25 14:49:24 -0500

    Update leveled_roles_setup.py

--- utils/global_cogs/leveled_roles_setup.py
+++ utils/global_cogs/leveled_roles_setup.py
@@ -117,7 +117,7 @@ class LeveledRolesCMD(commands.Cog):
             )
 
         # Reorder roles by level, with highest level role at the top
-        sorted_roles = sorted(created_roles.items(), key=lambda item: item[1], reverse=True)
+        sorted_roles = sorted(created_roles, key=lambda item: item[1], reverse=True)
         positions = {role: position for position, (role, _) in enumerate(sorted_roles, start=1)}
 
         await guild.edit_role_positions(positions=positions)
