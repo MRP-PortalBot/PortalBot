2af108d99355d2150de55980c66c95dc833efbe8
BSavage81 <beau.j.savage@gmail.com>
2024-10-01 10:03:00 -0500

    Update sync_slash.py

--- utils/global_cogs/sync_slash.py
+++ utils/global_cogs/sync_slash.py
@@ -49,35 +49,93 @@ class BackupRegularCommands(commands.Cog):
 
     @commands.command()
     @is_botAdmin3
-    async def sync(self, ctx: commands.Context, action: Union[Literal["global"], Literal["all"], discord.Guild]):
+    async def sync(
+        self,
+        ctx: commands.Context,
+        action: Union[Literal["global"], Literal["all"], discord.Guild],
+    ):
         if isinstance(action, discord.Guild):
             guild = action
-            message_sync = await self.send_sync_embed(ctx, "Sync", f"Syncing slash commands for guild `{guild.name}`...")
+
+            embed_processing = discord.Embed(
+                color=discord.Color.gold(),
+                title="Sync",
+                description=f"Syncing slash commands for guild `{guild.name}`...",
+            )
+            message_sync = await ctx.send(embed=embed_processing)
+
             await self.bot.tree.sync(guild=discord.Object(guild.id))
-            await message_sync.edit(embed=discord.Embed(title="Sync", description=f"Successfully synced for `{guild.name}`!", color=discord.Color.green()))
 
-        elif action == "global":
-            confirmation = await self.handle_confirmation(ctx, "Are you sure you want to sync globally? This may take up to 1 hour.")
-            if confirmation == "Confirm":
-                message_sync = await self.send_sync_embed(ctx, "Sync", "Syncing slash commands globally...\nThis may take a while.")
-                await self.bot.tree.sync()
-                await message_sync.edit(embed=discord.Embed(title="Sync", description="Successfully synced globally!", color=discord.Color.green()))
-            elif confirmation == "Cancel":
-                await self.send_sync_embed(ctx, "Sync", "Sync canceled.", Colors.red)
-            else:
-                await self.send_sync_embed(ctx, "Sync", "Sync canceled due to timeout.", Colors.red)
+            embed_done = discord.Embed(
+                color=discord.Color.green(),
+                title="Sync",
+                description=f"Successfully synced slash commands for guild `{guild.name}`!",
+            )
+            await message_sync.edit(embed=embed_done)
 
-        elif action == "all":
-            confirmation = await self.handle_confirmation(ctx, "Are you sure you want to sync all local guild commands?")
-            if confirmation == "Confirm":
-                message_sync = await self.send_sync_embed(ctx, "Sync", "Syncing all local guild slash commands...\nThis may take a while.")
-                for guild in self.bot.guilds:
-                    await self.bot.tree.sync(guild=discord.Object(guild.id))
-                await message_sync.edit(embed=discord.Embed(title="Sync", description="Successfully synced in all servers!", color=discord.Color.green()))
-            elif confirmation == "Cancel":
-                await self.send_sync_embed(ctx, "Sync", "Sync canceled.", Colors.red)
+        elif action == "global" or action == "all":
+            # Defer interaction to avoid timeout
+            await ctx.defer() 
+
+            view = ui.View(timeout=30)
+            button_confirm = ButtonHandler(
+                style=ButtonStyle.green,
+                label="Confirm",
+                emoji="✅",
+                button_user=ctx.author,
+            )
+            button_cancel = ButtonHandler(
+                style=ButtonStyle.red, label="Cancel", emoji="❌", button_user=ctx.author
+            )
+            view.add_item(button_confirm)
+            view.add_item(button_cancel)
+
+            embed_confirm = discord.Embed(
+                color=discord.Color.gold(),
+                title="Sync Confirmation",
+                description=f"Are you sure you want to sync globally? This may take up to 1 hour." if action == "global" else "Are you sure you want to sync all local guild commands?",
+            )
+            message_confirm = await ctx.send(embed=embed_confirm, view=view)
+
+            timeout = await view.wait()
+            if not timeout:
+                if view.value == "Confirm":
+
+                    embed_processing = discord.Embed(
+                        color=discord.Color.gold(),
+                        title="Sync",
+                        description=f"Syncing slash commands {action}..."
+                        f"\nThis may take a while.",
+                    )
+                    await message_confirm.edit(embed=embed_processing, view=None)
+
+                    if action == "global":
+                        await self.bot.tree.sync()
+                    else:
+                        for guild in self.bot.guilds:
+                            await self.bot.tree.sync(guild=discord.Object(guild.id))
+
+                    embed_processing = discord.Embed(
+                        color=discord.Color.green(),
+                        title="Sync",
+                        description=f"Successfully synced slash commands {action}!",
+                    )
+                    await message_confirm.edit(embed=embed_processing)
+
+                elif view.value == "Cancel":
+                    embed_cancel = discord.Embed(
+                        color=Colors.red, title="Sync", description="Sync canceled."
+                    )
+                    await message_confirm.edit(embed=embed_cancel, view=None)
+
             else:
-                await self.send_sync_embed(ctx, "Sync", "Sync canceled due to timeout.", Colors.red)
+                embed_timeout = discord.Embed(
+                    color=Colors.red,
+                    title="Sync",
+                    description="Sync canceled due to timeout.",
+                )
+                await message_confirm.edit(embed=embed_timeout, view=None)
+
 
 
 async def setup(bot):
