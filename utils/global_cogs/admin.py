62af0bf8cc69a4d0f0037d6c9f806ac7a4b9dece
BSavage81 <beau.j.savage@gmail.com>
2024-10-07 21:17:06 -0500

    update

---
+++ utils/global_cogs/admin.py
@@ -0,0 +1,100 @@
+import logging
+import discord
+from discord import app_commands
+from discord.ext import commands
+from pathlib import Path
+from core import database
+
+logger = logging.getLogger(__name__)
+
+
+class AdminCMD(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        logger.info("AdminCMD: Cog Loaded!")
+
+    # Grouping all admin-level commands
+    Admin = app_commands.Group(name="admin", description="Administrative commands.")
+
+    # Command to request the database file
+    @Admin.command(
+        name="requestdb", description="Request the database file for manual inspection"
+    )
+    @app_commands.checks.has_role("Bot Manager")
+    async def requestdb(self, interaction: discord.Interaction):
+        """Request the database file for manual inspection."""
+        try:
+            db = Path("data.db")
+            if not db.exists():
+                await interaction.response.send_message("Database does not exist yet.")
+                return
+            with db.open(mode="rb") as f:
+                file = discord.File(f, "database.db")
+                await interaction.user.send(file=file)
+            await interaction.response.send_message(
+                "Database file sent to your DMs.", ephemeral=True
+            )
+            logger.info(f"Database file sent to {interaction.user}")
+        except Exception as e:
+            logger.error(f"Error in sending the database file: {e}")
+            await interaction.response.send_message(
+                "An error occurred while sending the database file.", ephemeral=True
+            )
+
+    # Command to delete the database file
+    @Admin.command(name="deletedb", description="Delete the database file")
+    @app_commands.checks.has_role("Bot Manager")
+    async def deletedb(self, interaction: discord.Interaction):
+        """Delete the database file."""
+        try:
+            if database.db.is_closed():
+                db = Path("data.db")
+                if not db.exists():
+                    await interaction.response.send_message(
+                        "Database file does not exist."
+                    )
+                    return
+                db.unlink()
+                await interaction.response.send_message(
+                    "Database file has been deleted."
+                )
+                logger.info(f"Database file deleted by {interaction.user}")
+            else:
+                await interaction.response.send_message(
+                    "Cannot delete; database is currently in use."
+                )
+        except Exception as e:
+            logger.error(f"Error in deleting the database file: {e}")
+            await interaction.response.send_message(
+                "An error occurred while deleting the database file.", ephemeral=True
+            )
+
+    # Command to replace the database file with an attachment
+    @Admin.command(
+        name="replacedb", description="Replace the database file with attachment"
+    )
+    @app_commands.checks.has_role("Bot Manager")
+    async def replacedb(self, interaction: discord.Interaction):
+        """Replace the database file with an attachment."""
+        try:
+            if database.db.is_closed():
+                db = Path("data.db")
+                if db.exists():
+                    db.unlink()
+                with db.open(mode="wb+") as f:
+                    await interaction.message.attachments[0].save(f)
+                await interaction.response.send_message("Database file replaced.")
+                logger.info(f"Database file replaced by {interaction.user}")
+            else:
+                await interaction.response.send_message(
+                    "Cannot replace; database is currently in use."
+                )
+        except Exception as e:
+            logger.error(f"Error in replacing the database file: {e}")
+            await interaction.response.send_message(
+                "An error occurred while replacing the database file.", ephemeral=True
+            )
+
+
+async def setup(bot):
+    await bot.add_cog(AdminCMD(bot))
--- utils/global_cogs/misc.py
+++ utils/global_cogs/misc.py
@@ -1,17 +1,18 @@
 import ast
 import json
 import random
-from datetime import datetime
+import logging
+import time
+import psutil
+from datetime import datetime, timedelta
 from pathlib import Path
-
 import discord
-import requests
 from discord import app_commands
 from discord.ext import commands
 
 from core import database
 
-
+# List of server rules
 rules = [
     ":one: **No Harassment**, threats, hate speech, inappropriate language, posts or user names!",
     ":two: **No spamming** in chat or direct messages!",
@@ -24,33 +25,7 @@ rules = [
     ":nine: **Discord Terms of Service apply!** You must be at least **13** years old.",
 ]
 
-"""
-import sentry_sdk
-sentry_sdk.init(
-    "https://75b468c0a2e34f8ea4b724ca2a5e68a1@o500070.ingest.sentry.io/5579376",
-    traces_sample_rate=1.0
-)
-"""
-
-import logging
-
 logger = logging.getLogger(__name__)
-
-
-def insert_returns(body):
-    # insert return stmt if the last expression is a expression statement
-    if isinstance(body[-1], ast.Expr):
-        body[-1] = ast.Return(body[-1].value)
-        ast.fix_missing_locations(body[-1])
-
-    # for if statements, we insert returns into the body and the orelse
-    if isinstance(body[-1], ast.If):
-        insert_returns(body[-1].body)
-        insert_returns(body[-1].orelse)
-
-    # for with blocks, again we insert returns into the body
-    if isinstance(body[-1], ast.With):
-        insert_returns(body[-1].body)
 
 
 class MiscCMD(commands.Cog):
@@ -58,130 +33,107 @@ class MiscCMD(commands.Cog):
         self.bot = bot
         logger.info("MiscCMD: Cog Loaded!")
 
-    ##======================================================Commands===========================================================
-
-    # Nick Commamd
-    @commands.command()
-    @commands.has_role("Moderator")
-    async def nick(self, ctx, user: discord.Member, channel: discord.TextChannel):
-        author = ctx.message.author
-        name = user.display_name
-        channel = channel.name.split("-")
-        if len(channel) == 2:  # real-emoji
-            realm, emoji = channel
-        else:  # realm-name-emoji
-            realm, emoji = channel[0], channel[-1]
-        await user.edit(nick=str(name) + " " + str(emoji))
-        await ctx.send("Changed nickname!")
-
-    @nick.error
-    async def nick_error(self, ctx, error):
-        if isinstance(error, commands.MissingRole):
-            await ctx.send("Uh oh, looks like you don't have the Moderator role!")
-
-    @commands.command()
-    @commands.has_role("Bot Manager")
-    async def requestdb(self, ctx):
-        """Request the database file for manual inspection"""
-        db = Path("data.db")
-        if not db.exists():
-            await ctx.send("Database does not exist yet.")
-            return
-        with db.open(mode="rb") as f:
-            file = discord.File(f, "database.db")
-            await ctx.author.send(file=file)
-        await ctx.send("Database file sent to your DMs.")
-
-    @commands.command()
-    @commands.has_role("Bot Manager")
-    async def deletedb(self, ctx):
-        """Delete database file"""
-        if database.db.is_closed():
-            db = Path("data.db")
-            if not db.exists():
-                await ctx.send("Database file does not exist.")
-                return
-            db.unlink()
-            await ctx.send("Database file has been deleted.")
-        else:
-            await ctx.send("Cannot delete; database is currently in use.")
-
-    @commands.command()
-    @commands.has_role("Bot Manager")
-    async def replacedb(self, ctx):
-        """Replace database file with attachment"""
-        if database.db.is_closed():
-            db = Path("data.db")
-            if db.exists():
-                db.unlink()
-            with db.open(mode="wb+") as f:
-                await ctx.message.attachments.save(f)
-        else:
-            await ctx.send("Cannot replace; database is currently in use.")
-
     ##======================================================Slash Commands===========================================================
 
-    # Removes your nickname.
-    """@slash_command(name="rememoji", description = "Reverts your nickname back to your username!", guild_ids=[config['SlashServer1'],config['SlashServer2'],config['SlashServer3']])
-    async def removeemoji(self, ctx):
-        author = ctx.author
-        name = author.name
-        await author.edit(nick=str(name))
-        await ctx.respond(content = "Removed your nickname!")
+    # Nick Command
+    @app_commands.command(
+        name="nick",
+        description="Change a user's nickname based on the channel name emoji.",
+    )
+    @app_commands.describe(
+        user="The user whose nickname you want to change",
+        channel="The channel with the emoji in the name",
+    )
+    @app_commands.checks.has_role("Moderator")
+    async def nick(
+        self,
+        interaction: discord.Interaction,
+        user: discord.Member,
+        channel: discord.TextChannel,
+    ):
+        """Slash command to change a user's nickname based on the channel's emoji."""
+        try:
+            logger.info(
+                f"{interaction.user} is attempting to change the nickname of {user}"
+            )
+            name = user.display_name
+            channel_parts = channel.name.split("-")
 
-    # Add's an emoji to your nickname.
-    @slash_command(name="addemoji", description = "Add's an emoji to your nickname!", guild_ids=[config['SlashServer1'],config['SlashServer2'],config['SlashServer3']])
-    async def addemoji(self, ctx, channel: discord.TextChannel = None):
-        author = ctx.author
-        name = author.display_name
-        channel = channel.name.split('-')
-        if len(channel) == 2:  # real-emoji
-            realm, emoji = channel
-        else:  # realm-name-emoji
-            realm, emoji = channel[0], channel[-1]
-        await author.edit(nick=str(name) + str(emoji))
-        await ctx.respond(content = "Changed your nickname!")"""
+            if len(channel_parts) == 2:  # realm-emoji format
+                realm, emoji = channel_parts
+            else:  # realm-name-emoji format
+                realm, emoji = channel_parts[0], channel_parts[-1]
+
+            await user.edit(nick=f"{name} {emoji}")
+            await interaction.response.send_message(
+                f"Changed {user.mention}'s nickname!"
+            )
+
+        except Exception as e:
+            logger.error(f"Error in changing nickname: {e}")
+            await interaction.response.send_message(
+                f"An error occurred while changing the nickname.", ephemeral=True
+            )
 
     # Rule Command [INT]
     @app_commands.command(name="rule", description="Sends out MRP Server Rules")
     async def rule(self, interaction: discord.Interaction, number: int = None):
-        await interaction.response.send_message(rules[int(number) - 1])
+        try:
+            if 1 <= number <= len(rules):
+                await interaction.response.send_message(rules[number - 1])
+                logger.info(f"Rule {number} sent by {interaction.user}")
+            else:
+                await interaction.response.send_message(
+                    f"Please choose a valid rule number between 1 and {len(rules)}."
+                )
+        except Exception as e:
+            logger.error(f"Error in rule command: {e}")
+            await interaction.response.send_message(
+                "An error occurred while fetching the rule."
+            )
 
     # Ping Command
     @app_commands.command(description="Ping the bot")
     async def ping(self, interaction: discord.Interaction):
-        logger.info(f"Ping command called by {interaction.user}")
+        """Display the bot's ping and system resource usage."""
+        try:
+            logger.info(f"Ping command called by {interaction.user}")
+            uptime = timedelta(seconds=int(time.time() - self.bot.start_time))
+            ping_latency = round(self.bot.latency * 1000)
 
-        uptime = timedelta(seconds=int(time.time() - self.bot.start_time))
-        ping_latency = round(self.bot.latency * 1000)
+            pingembed = discord.Embed(
+                title="Pong! ⌛",
+                color=discord.Color.purple(),
+                description="Current Discord API Latency",
+            )
+            pingembed.set_author(name="PortalBot")
+            pingembed.add_field(
+                name="Ping & Uptime:",
+                value=f"```diff\n+ Ping: {ping_latency}ms\n+ Uptime: {str(uptime)}\n```",
+            )
 
-        pingembed = discord.Embed(
-            title="Pong! ⌛",
-            color=discord.Color.purple(),
-            description="Current Discord API Latency",
-        )
-        pingembed.set_author(name="PortalBot")
-        pingembed.add_field(
-            name="Ping & Uptime:",
-            value=f"```diff\n+ Ping: {ping_latency}ms\n+ Uptime: {str(uptime)}\n```",
-        )
+            # Adding system resource usage with more details
+            memory = psutil.virtual_memory()
+            pingembed.add_field(
+                name="System Resource Usage",
+                value=f"```diff\n- CPU Usage: {psutil.cpu_percent()}%\n- Memory Usage: {memory.percent}%\n"
+                f"- Total Memory: {memory.total / (1024**3):.2f} GB\n- Available Memory: {memory.available / (1024**3):.2f} GB\n```",
+                inline=False,
+            )
 
-        # Adding system resource usage with more details
-        memory = psutil.virtual_memory()
-        pingembed.add_field(
-            name="System Resource Usage",
-            value=f"```diff\n- CPU Usage: {psutil.cpu_percent()}%\n- Memory Usage: {memory.percent}%\n"
-            f"- Total Memory: {memory.total / (1024**3):.2f} GB\n- Available Memory: {memory.available / (1024**3):.2f} GB\n```",
-            inline=False,
-        )
+            pingembed.set_footer(
+                text=f"PortalBot Version: {self.bot.version}",
+                icon_url=interaction.user.display_avatar.url,
+            )
 
-        pingembed.set_footer(
-            text=f"PortalBot Version: {self.bot.version}",
-            icon_url=interaction.user.display_avatar.url,
-        )
-
-        await interaction.response.send_message(embed=pingembed)
+            await interaction.response.send_message(embed=pingembed)
+        except Exception as e:
+            logger.error(f"Error in ping command: {e}")
+            await interaction.response.send_message(
+                "An error occurred while fetching the ping information."
+            )
 
 
+# Set up the cog
 async def setup(bot):
     await bot.add_cog(MiscCMD(bot))
