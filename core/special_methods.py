d9a558abcab147c5e3c6777e611a258c8e82908f
BSavage81 <beau.j.savage@gmail.com>
2024-10-21 14:54:31 -0500

    Update special_methods.py

--- core/special_methods.py
+++ core/special_methods.py
@@ -52,7 +52,7 @@ async def on_ready_(bot: "PortalBot"):
             )
             continue
         initialize_persistent_views(bot, bot_data)
-        return bot_data
+        return
 
     # Determine the database source (external or local)
     database_source = "External" if not os.getenv("USEREAL") else "localhost"
@@ -117,7 +117,7 @@ async def on_ready_(bot: "PortalBot"):
         _log.info("Attempting to send sync message to 'github-log' channel.")
 
         # Fetch the guild object using the cached server ID
-        pb_guild = bot.get_guild(bot_data.pb_test_server_id)
+        pb_guild = bot.get_guild(get_cached_bot_data(bot.guilds).pb_test_server_id)
 
         if not pb_guild:
             _log.error(f"Guild with ID {bot_data.pb_test_server_id} not found.")
