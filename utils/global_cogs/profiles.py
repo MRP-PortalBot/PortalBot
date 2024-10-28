4c42935704cb98f1014b62883d8eda6cd2544637
BSavage81 <beau.j.savage@gmail.com>
2024-10-28 13:11:56 -0500

    Update profiles.py

--- utils/global_cogs/profiles.py
+++ utils/global_cogs/profiles.py
@@ -371,10 +371,10 @@ class ProfileCMD(commands.Cog):
         if query.RealmsJoined and query.RealmsJoined != "None":
             _log.info(f"{x},{y}")
             # Draw the title first
-            y + 25
+            y + 75
             member_realms_text = "Realms as Member:"
             self.draw_text_with_shadow(draw, x, y, member_realms_text, small_font)
-
+            _log.info(f"{x},{y}")
             # Update y-coordinate to add space below the title
             y += 25
 
