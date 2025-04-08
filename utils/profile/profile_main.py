572a82c98cc44421d9eba12d53332e6ff4c42e0c
BSavage81 <beau.j.savage@gmail.com>
2025-04-08 14:03:09 -0500

    update profile

--- utils/profile/profile_main.py
+++ utils/profile/profile_main.py
@@ -12,9 +12,9 @@ class ProfileCMD(commands.Cog):
 
 
 async def setup(bot):
-    from .profile_visual import add_visual_commands
-    from .profile_embed import add_embed_commands
-    from .profile_edit import add_edit_commands
+    from .__profile_visual import add_visual_commands
+    from .__profile_embed import add_embed_commands
+    from .__profile_edit import add_edit_commands
 
     # Create the top-level slash command group
     profile_group = app_commands.Group(
