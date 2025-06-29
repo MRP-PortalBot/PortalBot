c3edb89bf3dbc63b4cfacfda128ad0247828052e
BSavage81 <beau.j.savage@gmail.com>
2025-06-29 17:52:06 -0500

    update

--- utils/banned_list/bl_main.py
+++ utils/banned_list/bl_main.py
@@ -9,15 +9,6 @@ from utils.helpers.__logging_module import get_log
 _log = get_log(__name__)
 
 
-class BannedList(commands.GroupCog, name="banned-list"):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-
-
 async def setup(bot: commands.Bot):
-    cog = BannedList(bot)
-    await bot.add_cog(cog)
-
     await __bl_commands.setup(bot)
-
     _log.info("✅ Banned list system initialized.")
